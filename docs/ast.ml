97dfa6e3a3e8e255702d47b3d2ef32ce0ccf6a12
Shubham Joshi <shubhamjoshi@ucla.edu>
2016-06-12 17:29:32 -0700

    Fixed OCaml errors

--- docs/ast.ml
+++ docs/ast.ml
@@ -3,12 +3,14 @@ type ('a, 'b) either = Either of 'a  | Or of 'b
 
 type  var   = string
 
+type op = string
+
 type  value = Number    of float (*Worked well for JavaScript*)
             | Boolean   of bool 
             | String    of string
-            | Function  of (var list) * expression (*Arguments * body, not an interpreter so no closure*)
+            | Function  of (var list) * expr (*Arguments * body, not an interpreter so no closure*)
             | Array     of value list (*Converts to JS array, so linear access is still slow*)
-            | Dict      of (var, exp) list
+            | Dict      of (var * expr) list
             | Regexp    of string
             | Variable  of var
 
@@ -19,11 +21,11 @@ and  expr   = UndefLit  (*WHAT IS AN OPTION MONAD*)
             | Var       of var
             | ArrayLit  of expr list
             | OpExp     of value * op * value
-            | ObjLit    of (var * exp) list
-            | AssignExp of var * exp
-            | IfExp     of exp * exp * exp
-            | InvokeExp of exp * var * exp list (*Raise an error: You really shouldn't invoke a method in a language with no methods*)
-            | Lambda    of var list * expr
+            | ObjLit    of (var * expr) list
+            | AssignExp of var * expr
+            | IfExp     of expr * expr * expr
+            | InvokeExp of expr * var * (expr list) (*Raise an error: You really shouldn't invoke a method in a language with no methods*)
+            | Lambda    of (var list) * expr
             | Call      of expr * (expr list)
             | Raise     of event * bool
             | Alert     of event * bool
@@ -31,13 +33,13 @@ and  expr   = UndefLit  (*WHAT IS AN OPTION MONAD*)
 (*WHAT IS A TYPE SYSTEM*)
 and   react     = event * expr list
 and   intercept = event * expr list
-and   obj       = var * var * (react, intercept) either list
+and   obj       = var * var * ((react, intercept) either list)
 (*The big guy*)
-and   event     = (var, pattern) list 
+and   event     = (var * pattern) list 
 and   pattern   = Any
                 | ExactNum  of float
                 | ExactBool of bool
                 | ExactStr  of string
                 | ArrayPat  of pattern list
-                | ObjectPat of (var, pattern
+                | ObjectPat of (var * pattern) list
 
