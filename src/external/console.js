0189ebb385d0a2f7b817f309539cc22d74c70b32
Shubham Joshi <shubhamjoshi@ucla.edu>
2017-05-24 16:46:01 -0700

    Linted src/external/console.js

--- src/external/console.js
+++ src/external/console.js
@@ -1,14 +1,14 @@
+/*eslint no-console: 0*/
 (function(ct){
     var lg2console = {};
-    ["Log", "Error", "Warn"].forEach(function(f){
+    ['Log', 'Error', 'Warn'].forEach(function(f){
         ct.addListener({
-            event    : {name: new RegExp("Console::" + f)},
+            event    : {name: new RegExp('Console::' + f)},
             callback : function(e){
-                           console[f.toLowerCase()].
-                             apply(console, [e.data].concat(e.args||[]));
-                       },
+                console[f.toLowerCase()].apply(console, [e.data].concat(e.args||[]));
+            },
             thisArg  : lg2console,
-            id       : "lg2console"
+            id       : 'lg2console'
         });
     });
-}(container));
+}(__container));
